d2e52df84fee0f234289f92c34296129c6cc6778
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-08-23 11:57:27 -0400

    Takes one person in the elevator

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -4,4 +4,16 @@ class Elevator
   def initialize
     @total_stops = 0
   end
+
+  def elevator_stops(a, b)
+    if a.empty?
+      return @total_stops      
+    else
+      a.shift
+      b.shift
+
+      @total_stops += 1
+      elevator_stops(a, b)
+    end
+  end
 end
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -9,3 +9,20 @@ RSpec.describe Elevator do
     end
   end
 
+  describe '#elevator_stops' do
+    it 'if no one gets on, returns total_stops of 0' do
+      queue.elevator_stops([], [])
+
+      expect(queue.total_stops).to eq(0)
+    end
+
+    context 'one person gets on the elevator' do
+      it 'returns total_stops of 1' do
+        queue.elevator_stops([60], [2])
+
+        expect(queue.total_stops).to eq(1)
+      end
+
+    end
+  end
+end
