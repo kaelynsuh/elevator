f130aed445cc635b59a4b93af4dd2987b82fea8e
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-08-23 20:48:47 -0400

    2 people get on the elevator

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -1,25 +1,34 @@
 class Elevator
-  attr_accessor :total_stops
+  attr_accessor :total_stops, :current_people
 
   def initialize
     @total_stops = 0
+    @current_people = 0
   end
 
-  def elevator_stops(a, b, max_elevator_weight)
+  def elevator_stops(a, b, max_elevator_weight, max_people_allowed)
     if a.empty?
       return @total_stops      
     else
-      current_person_weight = a.shift
-      current_person_floor = b.shift
+      while (@current_people < max_people_allowed && a.any?)
 
-      if current_person_weight > max_elevator_weight
-        # dont incr
-      else
-        @total_stops += 1
+        current_person_weight = a.shift
+        current_person_floor = b.shift
+        @current_people += 1
       end
 
+      @total_stops += 1
+      @current_people = 0
+
+      # if current_person_weight > max_elevator_weight
+      #   a.shift
+      #   b.shift
+      #   # dont incr
+      # end
+
+      elevator_stops(a, b, max_elevator_weight, max_people_allowed)
     end
-    elevator_stops(a, b, max_elevator_weight)
   end
 end
 
+queue = Elevator.new
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -7,26 +7,39 @@ RSpec.describe Elevator do
     it 'initializes total_stops to 0' do
       expect(queue.total_stops).to eq(0)
     end
+
+    it 'initializes current_people to 0' do
+      expect(queue.current_people).to eq(0)
+    end
   end
 
   describe '#elevator_stops' do
     it 'if no one gets on, returns total_stops of 0' do
-      queue.elevator_stops([], [], 200)
+      queue.elevator_stops([], [], 200, 2)
 
       expect(queue.total_stops).to eq(0)
     end
 
     context 'one person gets on the elevator' do
       it 'returns total_stops of 1' do
-        queue.elevator_stops([60], [2], 200)
+        queue.elevator_stops([60], [2], 200, 2)
 
         expect(queue.total_stops).to eq(1)
       end
 
-      it 'doesnt take the person if current_person_weight exceeds the max_elevator_weight' do
-        queue.elevator_stops([250], [2], 200)
+      # maybe dont check for this..
+      # it 'doesnt take the person if current_person_weight exceeds the max_elevator_weight' do
+      #   queue.elevator_stops([250], [2], 200, 2)
 
-        expect(queue.total_stops).to eq(0)
+      #   expect(queue.total_stops).to eq(0)
+      # end
+    end
+
+    context 'two people get on the elevator' do
+      it 'if max_people_allowed is 2, returns total_stops of 1' do
+        queue.elevator_stops([60, 80], [2, 3], 200, 2)
+
+        expect(queue.total_stops).to eq(1)        
       end
     end
   end
