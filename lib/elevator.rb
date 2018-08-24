f33654db24141f9e809faa96f729d9efbf1946f7
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-08-24 12:49:45 -0400

    Remove global variables, inits, and refactor

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -1,39 +1,26 @@
 class Elevator
-  attr_accessor :total_stops, :current_people, :current_weight, :floors
-
-  def initialize
-    @total_stops = 0
-    @current_people = 0
-    @current_weight = 0
-    @floors = Hash.new(0)
-  end
-
   def elevator_stops(a, b, elevator_cap, people_cap)
+    floors = Hash.new(0)
+    stops = 0
+
     if a.empty?
-      return @total_stops
+      return 0
     elsif a[0] > elevator_cap
       a.shift
       b.shift
     else
-      while (@current_people < people_cap && !a.empty? && @current_weight + a[0] <= elevator_cap)
-        @current_people += 1
-        @current_weight += a.shift
-        @floors[b.shift] += 1
+      current_people = 0
+      current_weight = 0
+      while (current_people < people_cap &&
+             !a.empty? && 
+             current_weight + a[0] <= elevator_cap)
+        current_people += 1
+        current_weight += a.shift
+        floors[b.shift] += 1
       end
-
-      reset()
+      stops += 1
     end
 
-    elevator_stops(a, b, elevator_cap, people_cap)
-  end
-
-  def reset()
-    @current_people = 0
-    @current_weight = 0
-    @total_stops += @floors.count + 1
-    @floors = Hash.new(0)
+    elevator_stops(a, b, elevator_cap, people_cap) + floors.count + stops
   end
 end
-
-# queue = Elevator.new
-# puts queue.elevator_stops([60, 80, 40], [2, 3, 5], 200, 2)
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -3,70 +3,58 @@ require 'elevator'
 RSpec.describe Elevator do
   let(:queue) { Elevator.new }
 
-  describe '#initialize' do
-    it 'initializes total_stops to 0' do
-      expect(queue.total_stops).to eq(0)
-    end
-
-    it 'initializes current_people to 0' do
-      expect(queue.current_people).to eq(0)
-    end
-
-    it 'initializes current_weight to 0' do
-      expect(queue.current_weight).to eq(0)
-    end
-
-    it 'initializes floors to empty hash with 0 values' do
-      expect(queue.floors).to eq({})
-    end
-  end
-
   describe '#elevator_stops' do
     it 'if no one gets on, returns total_stops of 0' do
-      queue.elevator_stops([], [], 200, 2)
+      total_stops = queue.elevator_stops([], [], 200, 2)
 
-      expect(queue.total_stops).to eq(0)
+      expect(total_stops).to eq(0)
     end
 
     context 'one person gets on the elevator' do
       it 'returns total_stops of 2' do
-        queue.elevator_stops([60], [2], 200, 2)
-
-        expect(queue.total_stops).to eq(2)
+        total_stops = queue.elevator_stops([60], [2], 200, 2)
+        
+        expect(total_stops).to eq(2)
       end
 
       it 'doesnt take the person if persons weight exceeds the max_elevator_weight' do
-        queue.elevator_stops([250], [2], 200, 2)
+        total_stops = queue.elevator_stops([250], [2], 200, 2)
 
-        expect(queue.total_stops).to eq(0)
+        expect(total_stops).to eq(0)
       end
     end
 
     context 'two people get on the elevator' do
       it 'if people_cap is 2, returns total_stops of 3' do
-        queue.elevator_stops([60, 80], [2, 3], 200, 2)
+        total_stops = queue.elevator_stops([60, 80], [2, 3], 200, 2)
 
-        expect(queue.total_stops).to eq(3)
+        expect(total_stops).to eq(3)
       end
 
       it 'if 2 people exceed elevator_cap, they get put on separate elevators, with total_stops of 4' do
-        queue.elevator_stops([180, 100], [2, 3], 200, 2)
+        total_stops = queue.elevator_stops([180, 100], [2, 3], 200, 2)
 
-        expect(queue.total_stops).to eq(4) 
+        expect(total_stops).to eq(4) 
       end
 
       it 'if 2 people get off the same floor, returns total_stops of 1' do
-        queue.elevator_stops([60, 80], [2, 2], 200, 2)
+        total_stops = queue.elevator_stops([60, 80], [2, 2], 200, 2)
 
-        expect(queue.total_stops).to eq(2)
+        expect(total_stops).to eq(2)
       end
     end
 
     context 'three people get on the elevator' do
       it 'after an elevator ride of 2 people, elevator will reach ground to pick up the next person, drop them off, then reach ground again' do
-        queue.elevator_stops([60, 80, 40], [2, 3, 5], 200, 2)
+        total_stops = queue.elevator_stops([60, 80, 40], [2, 3, 5], 200, 2)
 
-        expect(queue.total_stops).to eq(5)
+        expect(total_stops).to eq(5)
+      end
+
+      it 'it has the person exceeding elevator weight first in queue' do
+        total_stops = queue.elevator_stops([2000, 80, 100], [2, 3, 4], 200, 2)
+
+        expect(total_stops).to eq(3)
       end
     end
   end
